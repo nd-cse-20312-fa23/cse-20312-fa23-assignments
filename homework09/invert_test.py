56e5dc16c84b9bd246f0a5c5581be8f2c81ead0f
Peter Bui <pbui@nd.edu>
2023-10-26 22:05:23 -0400

    Homework 09: whitespace

--- homework09/invert_test.py
+++ homework09/invert_test.py
@@ -21,7 +21,7 @@ class InvertTests(unittest.TestCase):
         ([1, 2, 3, 4, 0, 0, 6], [1, 3, 2, 6, 0, 0, 4]),
         ([9, 2, 8, 6, 0, 7]   , [9, 8, 2, 0, 7, 0, 6]),
     ]
-    
+
     @classmethod
     def setupClass(cls):
         cls.Points = 0
@@ -37,7 +37,7 @@ class InvertTests(unittest.TestCase):
         self.assertEqual(failures, 0)
         self.assertEqual(tests, 3)
         InvertTests.Points += 0.25
-    
+
     def test_01_mypy(self):
         _, _, exit_status = mypy.api.run(['invert.py'])
         self.assertTrue(exit_status == 0)
@@ -49,7 +49,7 @@ class InvertTests(unittest.TestCase):
             inverted = tree_read(inverted)
             self.assertEqual(invert.tree_invert(original), inverted)
             InvertTests.Points += 0.25
-    
+
     def test_03_main(self):
         case_input = io.StringIO('\n'.join(' '.join(map(str, o)) for o, _ in self.Cases))
         with unittest.mock.patch('sys.stdout', new=io.StringIO()) as output:
@@ -64,7 +64,7 @@ class InvertTests(unittest.TestCase):
         for line, case_output in zip(lines, case_output):
             self.assertEqual(line, case_output)
             InvertTests.Points += 0.125
-        
+
 # Main Execution
 
 if __name__ == '__main__':
--- homework09/tree_test.py
+++ homework09/tree_test.py
@@ -17,7 +17,7 @@ class TreeTests(unittest.TestCase):
 
     Tree1  = Node(value=1, left=Node(value=2, left=None, right=None), right=Node(value=3, left=None, right=None))
     Tree2  = Node(value=1, left=Node(value=2, left=Node(value=4, left=None, right=None), right=None), right=Node(value=3, left=None, right=Node(value=6, left=None, right=None)))
-    
+
     @classmethod
     def setupClass(cls):
         cls.Points = 0
@@ -33,7 +33,7 @@ class TreeTests(unittest.TestCase):
         self.assertEqual(failures, 0)
         self.assertEqual(tests, 3)
         TreeTests.Points += 0.5
-    
+
     def test_01_mypy(self):
         _, _, exit_status = mypy.api.run(['tree.py'])
         self.assertTrue(exit_status == 0)
@@ -44,34 +44,34 @@ class TreeTests(unittest.TestCase):
         self.assertTrue(isinstance(root, Node))
         self.assertEqual(root, self.Tree1)
         TreeTests.Points += 0.25
-        
+
         root = tree_read([1, 2, 3, 4, 0, 0, 6])
         self.assertTrue(isinstance(root, Node))
         self.assertEqual(root, self.Tree2)
         TreeTests.Points += 0.25
-    
+
     def test_03_tree_values(self):
         values = tree_values(self.Tree1)
         self.assertTrue(isinstance(values, types.GeneratorType))
         self.assertEqual(list(values), [2, 1, 3])
         TreeTests.Points += 0.25
-        
+
         values = tree_values(self.Tree2)
         self.assertTrue(isinstance(values, types.GeneratorType))
         self.assertEqual(list(values), [4, 2, 1, 3, 6])
         TreeTests.Points += 0.25
-    
+
     def test_04_tree_array(self):
         array = tree_array(self.Tree1)
         self.assertTrue(isinstance(array, list))
         self.assertEqual(array, [1, 2, 3])
         TreeTests.Points += 0.5
-        
+
         array = tree_array(self.Tree2)
         self.assertTrue(isinstance(array, list))
         self.assertEqual(array, [1, 2, 3, 4, 0, 0, 6])
         TreeTests.Points += 0.5
-        
+
 # Main Execution
 
 if __name__ == '__main__':
