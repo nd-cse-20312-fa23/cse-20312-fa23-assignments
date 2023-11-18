c220de402b4d7adc0bf98183d2af3a29342bd751
Peter Bui <pbui@nd.edu>
2023-11-18 17:52:50 -0500

    Homework 12: update graph test

--- homework12/graph_test.py
+++ homework12/graph_test.py
@@ -35,9 +35,23 @@ class GraphTests(unittest.TestCase):
 
     Graph4 = '4\n3\n1 2\n2 3\n4 1\n'
     AL4    = {1: [2, 4], 2: [1, 3], 3: [2], 4: [1]}
-    
+
     Graph5 = '4\n2\n1 2\n3 4\n'
     AL5    = {1: [2], 2: [1], 3: [4], 4: [3]}
+
+    Graph6 = '10\n7\n1 2\n6 8\n8 1\n10 6\n7 5\n3 6\n6 2\n'
+    AL6    = {
+        1: [2, 8],
+        2: [1, 6],
+        3: [6],
+        4: [],
+        5: [7],
+        6: [8, 10, 3, 2],
+        7: [5],
+        8: [6, 1],
+        9: [],
+        10: [6]
+    }
 
     @classmethod
     def setupClass(cls):
@@ -59,7 +73,7 @@ class GraphTests(unittest.TestCase):
         _, _, exit_status = mypy.api.run(['graph.py'])
         self.assertTrue(exit_status == 0)
         GraphTests.Points += 0.5
-    
+
     def test_02_read_adjacency_matrix(self):
         cases = (
             (io.StringIO(self.Graph1), self.AM1),
@@ -70,20 +84,21 @@ class GraphTests(unittest.TestCase):
         for stream, adj_matrix in cases:
             self.assertEqual(read_adjacency_matrix(stream), adj_matrix)
             GraphTests.Points += 1/len(cases)
-    
+
     def test_03_read_adjacency_list(self):
         cases = (
             (io.StringIO(self.Graph4), self.AL4),
             (io.StringIO(self.Graph5), self.AL5),
+            (io.StringIO(self.Graph6), self.AL6),
         )
 
         for stream, adj_list in cases:
             self.assertEqual(read_adjacency_list(stream), adj_list)
             GraphTests.Points += 1/len(cases)*0.5
-    
+
     def test_04_read_adjacency_lists(self):
-        stream    = io.StringIO(self.Graph4 + self.Graph5)
-        expecteds = (self.AL4, self.AL5)
+        stream    = io.StringIO(self.Graph4 + self.Graph5 + self.Graph6)
+        expecteds = (self.AL4, self.AL5, self.AL6)
 
         for result, expected in zip_longest(read_adjacency_lists(stream), expecteds):
             self.assertEqual(result, expected)
